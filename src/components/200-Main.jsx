b52cd2d34d751c1a449337789eb5b733372005a1
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-21 19:31:26 +0100

    Input form

--- src/components/200-Main.jsx
+++ src/components/200-Main.jsx
@@ -1,11 +1,30 @@
-import { Container } from "react-bootstrap"
+import { Button, Container, Form } from "react-bootstrap"
 
 
 const Main=()=>{
 
     return(
-        <Container className='bg-dark vh-100 mt-3'>
-            
+        <Container className='bg-dark vh-100 mt-5'>
+            <Form className=''>
+                <Form.Group className="mb-4 text-light w-50 mx-auto" controlId="formBasicEnter">
+                    <Form.Label className='badge bg-dark border border-light text-wrap py-2 px-3 mt-2 fs-5'>hex string:</Form.Label>
+                    <Form.Control type="text" />
+                    <Form.Text className="text-muted">
+                    Enter hex to convert
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Select aria-label="Select type of convertion" className='w-50 mx-auto'>
+                    <option value="1">From hex to base64</option>
+                    <option value="2">From base64 to hex</option>
+                </Form.Select>
+
+                <Container fluid className='mt-5 w-50'>
+                    <Button variant="warning" type="submit" className='w-100'>  
+                        Convert!
+                    </Button>
+                </Container>
+            </Form>
         </Container>
     )    
 }
