f4ea04fb8bbd4e5ad2cd6a4f0cb36b8634ac93e7
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-21 21:15:24 +0100

    Js converter + React logic about it

--- src/components/200-Main.jsx
+++ src/components/200-Main.jsx
@@ -1,30 +1,54 @@
-import { Button, Container, Form } from "react-bootstrap"
+import { useState } from "react"
+import { Alert, Button, Container, Form } from "react-bootstrap"
+import { base64ToHex, hexToBase64 } from "../utils/converter"
 
 
 const Main=()=>{
+    const[direction,setDirection]=useState('hexToBase64')
+    const[inputValue,setInputValue]=useState()
+    const handleInputValue=(e)=>{
+        setInputValue(e.target.value)
+    }
+    const[result,setResult]=useState('')
+    const convert=(e)=>{
+        e.preventDefault()
+        // console.log(inputValue)
+        const res=direction==='hexToBase64'?hexToBase64({inputValue,setResult}):base64ToHex({inputValue,setResult})
+        // setResult(res)
+    }
 
     return(
         <Container className='bg-dark vh-100 mt-5'>
+
             <Form className=''>
+
                 <Form.Group className="mb-4 text-light w-50 mx-auto" controlId="formBasicEnter">
                     <Form.Label className='badge bg-dark border border-light text-wrap py-2 px-3 mt-2 fs-5'>hex string:</Form.Label>
-                    <Form.Control type="text" />
+                    <Form.Control type="text" onChange={(e)=>handleInputValue(e)} />
                     <Form.Text className="text-muted">
                     Enter hex to convert
                     </Form.Text>
                 </Form.Group>
 
                 <Form.Select aria-label="Select type of convertion" className='w-50 mx-auto'>
-                    <option value="1">From hex to base64</option>
-                    <option value="2">From base64 to hex</option>
+                    <option onClick={()=>setDirection('hexToBase64')}>From hex to base64</option>
+                    <option onClick={()=>setDirection('base64ToHex')}>From base64 to hex</option>
                 </Form.Select>
 
                 <Container fluid className='mt-5 w-50'>
-                    <Button variant="warning" type="submit" className='w-100'>  
+                    <Button variant="warning" type="submit" className='w-100' onClick={(e)=>convert(e)}>  
                         Convert!
                     </Button>
                 </Container>
+
             </Form>
+
+            {result&&
+                <Alert className='bg-light mt-5 w-50 mx-auto'>
+                    {result}
+                </Alert>
+            }
+
         </Container>
     )    
 }
---
+++ src/utils/converter.js
@@ -0,0 +1,12 @@
+import {Buffer} from 'buffer';
+
+const STRING='4a1b48d7322394a78326cd283f0834093e65421a7e98e90e5b452d160b5d07d8'
+
+export const hexToBase64=({inputValue,setResult})=>{
+    setResult(Buffer.from(inputValue).toString('base64'))
+}
+
+
+export const base64ToHex=({inputValue,setResult})=>{
+    setResult(Buffer.from(inputValue, 'base64').toString())
+}
