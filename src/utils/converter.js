22dca5e6c8d7654cc5fff0d27259f4247561d7f4
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-25 21:23:00 +0100

    Background fade

--- src/utils/converter.js
+++ src/utils/converter.js
@@ -1,6 +1,6 @@
 import {Buffer} from 'buffer';
 
-//? const STRING_FROM_TASK='4a1b48d7322394a78326cd283f0834093e65421a7e98e90e5b452d160b5d07d8'
+//? const HEX_EXAMPLE='4a1b48d7322394a78326cd283f0834093e65421a7e98e90e5b452d160b5d07d8'
 
 export const hexToBase64=({inputValue,setResult})=>{
     setResult(Buffer.from(inputValue,'hex').toString('base64'))
